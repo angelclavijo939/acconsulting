598754f7646e449457e88f31971836b9d45b7cad
angelclavijo939 <angel.clavijor@gmail.com>
2026-04-23 13:05:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -179,7 +179,7 @@ if (contactForm) {
     }
 
     try {
-      const res = await fetch('../backend/contact.php', {
+      const res = await fetch('/backend/contact.php', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
